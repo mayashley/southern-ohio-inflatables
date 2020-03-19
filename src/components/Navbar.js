1e1037303cd21b7b0ebe0a042ec8024b33e570ea
Ashley <mayashley052@gmail.com>
2020-03-19 12:10:07 -0400

    errorfix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,36 +4,11 @@ import links from "../constants/links"
 import SocialIcons from "../constants/social-icons"
 import styles from "../css/navbar.module.css"
 import { Link } from "gatsby"
+
+
 const Navbar = () => {
-  const [isOpen, setNav] = useState(false)
-  const toggleNav = () => {
-    setNav(isOpen => !isOpen)
-  }
-  return (
-    <nav className={styles.navbar}>
-      <div className={styles.navCenter}>
-        <div className={styles.navHeader}>
-          <button type="button" className={styles.logoBtn} onClick={toggleNav}>
-            <FaAlignRight className={styles.logoIcon} />
-          </button>
-        </div>
-        <ul
-          className={
-            isOpen
-              ? `${styles.navLinks} ${styles.showNav}`
-              : `${styles.navLinks}`
-          }
-        >
-         {links.map((item, index) =>{
-           return (
-             <li key={index}>
-               <Link to={item.path}>{item.text}</Link>
-             </li>
-           )
-         })}
-        </ul>
-      </div>
-    </nav>
-  )
+<nav>
+
+</nav>
 }
 export default Navbar
