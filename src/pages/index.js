512443cf5b5067063d0dad8da15468ac99c4e9f5
Ashley <mayashley052@gmail.com>
2020-03-21 13:37:20 -0400

    created new component for banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import React from "react"
-import Layout from '../components/Layout'
-import Hero from '../components/Hero'
+import Layout from "../components/Layout"
+import Hero from "../components/Hero"
+import Banner from "../components/Banner"
 
 export default () => (
-    <Layout>
-        <Hero/>
-    </Layout>
+  <Layout>
+    <Hero>
+      <Banner
+        title="Southern Ohio Inflatables LLC."
+        info="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer imperdiet bibendum metus, sit amet varius ex hendrerit eu."
+      />
+    </Hero>
+  </Layout>
 )
