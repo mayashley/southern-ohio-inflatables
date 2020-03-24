3bfd1aab3f22c641aab00484d8af517561978e9a
Ashley <mayashley052@gmail.com>
2020-03-23 20:47:23 -0400

    fix attempt 3

--- src/components/simpleHero.js
+++ src/components/simpleHero.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from '../css/simpleHero.module.css'
-import aboutImg from '../images/aboutImg.jpeg'
+// import aboutImg from '../images/aboutImg.jpeg'
 
 const SimpleHero = () => {
     return (
         <div className={styles.simpleHeroWrapper}>
-           <img src={aboutImg} alt="faq link hero"/> 
+           {/* <img src={aboutImg} alt="faq link hero"/>  */}
         </div>
     )
 }
