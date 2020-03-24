b7201c73a92ada2f3419700b4f985a47da52c419
Ashley <mayashley052@gmail.com>
2020-03-23 23:32:54 -0400

    added image query to hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import {useStaticQuery, graphql} from 'gatsby'
+import Img from 'gatsby-image'
+
+
+const getHomeImg = graphql`
+query homeImg{
+    homeImg:file(relativePath:{eq:"bounceHouse2.png"}){
+      childImageSharp{
+        fluid{
+          src
+        }
+      }
+    }
+  }
+`
+
 
  const Hero = ({children}) => {
+    const{homeImg} = useStaticQuery(getHomeImg);
     return (
         <header className='defaultHero'>
+             <Img fluid={homeImg.childImageSharp.fluid} alt="kids bouncing"/>
             {children}
         </header>
     )
