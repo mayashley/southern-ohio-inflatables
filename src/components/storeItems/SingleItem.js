797792f2edcc476486c4d2ef2962223fe0acd764
Ashley <mayashley052@gmail.com>
2020-03-26 17:37:17 -0400

    added images for featued items

--- src/components/storeItems/SingleItem.js
+++ src/components/storeItems/SingleItem.js
@@ -1,13 +1,36 @@
 import React from 'react'
-// import 
+import Image from 'gatsby-image'
+import styles from "../../css/singleItem.module.css"
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
 
 
 
- const SingleItem = () => {
+ const SingleItem = ({item}) => {
+   const {name, price, slug, images} = item
+let mainImage = images[0].fluid;
+
+
   return (
-    <div>
-      this is a single combo
-    </div>
+  <article className={styles.tour}>
+   
+<div classname={styles.imgContainer}>
+  <Image fluid={mainImage} className={styles.img} alt="single item">
+<AniLink fade className={styles.link} to={`/BounceCombo/${slug}`}>
+  Details
+</AniLink>
+</Image>
+</div>
+<div className={styles.footer}>
+  <h3>{name}</h3>
+  <div className={styles.info}>
+    <h5 className={styles.details}>
+       Price: ${price}
+    </h5>
+  </div>
+</div>
+
+  </article>
+
   )
 }
 export default SingleItem
