6b9b9e960cf99465bd2293a135cefe7fab27d73d
Ashley <mayashley052@gmail.com>
2020-03-27 15:26:32 -0400

    working on title component and css for pages

--- src/pages/BounceCombo.js
+++ src/pages/BounceCombo.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import AllItems from '../components/storeItems/AllItems'
+import Title from '../constants/Title'
+import styles from '../css/items.module.css'
 
 
  const BounceCombo = () => {
     return (
         <Layout>
+             <div className={styles.itemsWrapper}>
+            <Title  title="Bounce House Combo's"/>
          <AllItems/>
+         </div>
         </Layout>
     )
 }
--- src/pages/ObstacleCourse.js
+++ src/pages/ObstacleCourse.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import styles from '../css/items.module.css'
 import ItemsList from '../components/storeItems/ItemsList'
 import {useStaticQuery, graphql} from 'gatsby'
+import Title from '../constants/Title'
 
 const getAllObstacles = graphql`
 query{
@@ -28,7 +30,10 @@ query{
     const { combos } = useStaticQuery(getAllObstacles);
     return (
         <Layout>
+            <div className={styles.itemsWrapper}>
+            <Title  title="Obstacle Course's"/>
            <ItemsList combos={combos}/>
+           </div>
         </Layout>
     )
 }
