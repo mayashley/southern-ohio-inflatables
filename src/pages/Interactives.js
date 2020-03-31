0ffc8fe2f201707f69732defab038f2826f6efd7
Ashley <mayashley052@gmail.com>
2020-03-31 14:56:12 -0400

    added items to the inflatable interactives

--- src/pages/Interactives.js
+++ src/pages/Interactives.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import {useStaticQuery, graphql} from 'gatsby'
+import Title from '../constants/Title'
+import styles from '../css/items.module.css'
+import ItemsList from '../components/storeItems/ItemsList'
+
+
+const getAllInteractives = graphql`
+query{
+    combos:allContentfulInteractives{
+      edges{
+        node{
+          name
+          price
+          slug
+          contentful_id
+          images{
+            fluid{
+                ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 
  const Interactives = () => {
+    const { combos } = useStaticQuery(getAllInteractives);
     return (
         <Layout>
-           Interactives
+            <div className={styles.itemsWrapper}>
+            <Title  title="Inflatable Interactives"/>
+           <ItemsList combos={combos}/>
+           </div>
         </Layout>
     )
 }
