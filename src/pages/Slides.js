7033eda7e893d68b664661b7d00b7dd341aa8911
Ashley <mayashley052@gmail.com>
2020-03-31 14:09:44 -0400

    added slides to slide page

--- src/pages/Slides.js
+++ src/pages/Slides.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import {useStaticQuery, graphql} from 'gatsby'
+import Title from '../constants/Title'
+import styles from '../css/items.module.css'
+import ItemsList from '../components/storeItems/ItemsList'
+
+
+const getAllSlides = graphql`
+query{
+    combos:allContentfulSlides{
+      edges{
+        node{
+          name
+          price
+          slug
+          contentful_id
+          images{
+            fluid{
+                ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 const Slides = () => {
+    const { combos } = useStaticQuery(getAllSlides);
     return (
        <Layout>
-           slides page
+             <div className={styles.itemsWrapper}>
+            <Title  title="Slides"/>
+           <ItemsList combos={combos}/>
+           </div>
        </Layout>
     )
 }
