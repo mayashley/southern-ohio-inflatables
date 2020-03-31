8f5613c40f1f1ad47e7875a828dccaf338e8915d
Ashley <mayashley052@gmail.com>
2020-03-31 15:01:37 -0400

    added the inflatable interactives to pages for viewing

--- src/components/storeItems/AllItems.js
+++ src/components/storeItems/AllItems.js
@@ -1,35 +1,30 @@
-import React from 'react'
-import ItemsList from './ItemsList'
-import {useStaticQuery, graphql} from 'gatsby'
-
+import React from "react"
+import ItemsList from "./ItemsList"
+import { useStaticQuery, graphql } from "gatsby"
 
 const getAllCombos = graphql`
-query{
-    combos:allContentfulCombos{
-      edges{
-      node{
-        name
-        price
-        slug
-        contentful_id
-        images{
-          fluid{
-            ...GatsbyContentfulFluid_tracedSVG
+  query {
+    combos: allContentfulCombos {
+      edges {
+        node {
+          name
+          price
+          slug
+          contentful_id
+          images {
+            fluid {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
           }
         }
-      }
       }
     }
   }
 `
 
+const AllItems = () => {
+  const { combos } = useStaticQuery(getAllCombos)
 
- const AllItems = () => {
-     const {combos} = useStaticQuery(getAllCombos);
-
-    return (
-   <ItemsList combos={combos}/>
-       
-    )
+  return <ItemsList combos={combos} />
 }
 export default AllItems
